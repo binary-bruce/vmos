5a90f0342e74c71cda3371636fe601cf95d4e4d1
binary.bruce <myth.litao@foxmail.com>
2024-07-21 16:13:19 +0800

    address utility

--- os/src/mm/address.rs
+++ os/src/mm/address.rs
@@ -3,13 +3,15 @@ use crate::config::PAGE_SIZE_BITS;
 // re-export them all
 pub use pa::*;
 pub use ppn::*;
+pub use range::*;
 pub use va::*;
 pub use vpn::*;
 
-pub mod pa;
-pub mod ppn;
-pub mod va;
-pub mod vpn;
+mod pa;
+mod ppn;
+mod range;
+mod va;
+mod vpn;
 
 const PA_WIDTH_SV39: usize = 56;
 const PPN_WIDTH_SV39: usize = PA_WIDTH_SV39 - PAGE_SIZE_BITS; // 56 - 12 == 44
