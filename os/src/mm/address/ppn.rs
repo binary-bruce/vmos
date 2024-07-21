7da2de8541c825cbdc228f0229cecafb8d4aa9cc
binary.bruce <myth.litao@foxmail.com>
2024-07-21 11:38:41 +0800

    page table entry

--- os/src/mm/address/ppn.rs
+++ os/src/mm/address/ppn.rs
@@ -2,6 +2,8 @@
 
 use core::fmt::{Debug, Formatter, Result};
 
+use crate::mm::page_table::PageTableEntry;
+
 use super::{pa::PhysAddr, PPN_WIDTH_SV39};
 
 /// physical page number
@@ -30,5 +32,25 @@ impl From<PhysAddr> for PhysPageNum {
     fn from(v: PhysAddr) -> Self {
         assert_eq!(v.page_offset(), 0);
         v.floor()
+    }
+}
+
+impl PhysPageNum {
+    pub fn get_pte_array(&self) -> &'static mut [PageTableEntry] {
+        let pa: PhysAddr = (*self).into();
+        let ptr = pa.0 as *mut PageTableEntry;
+        unsafe { core::slice::from_raw_parts_mut(ptr, 512) }
+    }
+
+    pub fn get_bytes_array(&self) -> &'static mut [u8] {
+        let pa: PhysAddr = (*self).into();
+        let ptr = pa.0 as *mut u8;
+        unsafe { core::slice::from_raw_parts_mut(ptr, 4096) }
+    }
+
+    pub fn get_mut<T>(&self) -> &'static mut T {
+        let pa: PhysAddr = (*self).into();
+        let ptr = pa.0 as *mut T;
+        unsafe { ptr.as_mut().unwrap() }
     }
 }
